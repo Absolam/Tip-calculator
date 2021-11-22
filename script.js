06be50e2b3134e8f27d711de03dda3ef4b3481eb
Aaron <78183996+Absolam@users.noreply.github.com>
2021-11-22 18:34:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ let totalAmount = document.querySelector(".total-amount");
 
 function calculateTip(num1) {
   let x = (billInput.value * num1) / peopleInput.value;
+  x = x.toFixed(2);
   tipAmount.textContent = x;
-  totalAmount.textContent = billInput.value / peopleInput.value;
+  let xx = billInput.value / peopleInput.value;
+  xx = xx.toFixed(2);
+  totalAmount.textContent = xx;
 }
 
 function reset() {
